ca25af20205eb8e7cb2b952b26e8a6aac41e4750
Cédric <cedric.milius@yahoo.fr>
2020-06-06 18:07:54 +0200

    move api call to pass test

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -3,15 +3,16 @@ import './App.css';
 
 function ItemDetail({match}) {
     useEffect(() => {
+        const fetchItem = async () => {
+            const response = await fetch(`https://cwb-server.herokuapp.com/api/v1/posts/${match.params.id}`)
+            const data = await response.json();
+            setItem(data.item);
+
+        };
         fetchItem();
     }, []);
     const [item, setItem] = useState({})
-    const fetchItem = async() => {
-        const response = await fetch(`https://cwb-server.herokuapp.com/api/v1/posts/${match.params.id}`)
-        const data = await response.json();
-        setItem(data.item);
-     
-    }
+    
     return (
         <div >
             <h1>{item && item.title ? item.title : <p>Loading...</p>}</h1>
