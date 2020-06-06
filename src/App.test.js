b12edf64018488ade0fbbf879fa2522ebd32c64f
Cédric <cedric.milius@yahoo.fr>
2020-06-06 17:46:29 +0200

    first test with @testing-library/react

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+function renderComponent() {
+  return render(
+    <App />
+  );
+}
+
+describe("<App>", () => {
+  it('should renders without crashing', () => {
+    renderComponent();
+  });
 });
