6bf2948711e1b3231f8d46551348d41bcf33c2c4
Cédric <cedric.milius@yahoo.fr>
2020-06-07 19:30:33 +0200

    authenticated

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 //import auth from "./pages/Login/Auth";
 
 
